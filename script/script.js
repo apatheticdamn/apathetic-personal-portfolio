de59e80631c96f55c8af254e22e131e0ed622fe7
apatheticdamn <apathetic.damn@gmail.com>
2024-01-30 21:42:08 +0530

    rename: projects to skills (nav bar) add: new look about section remove: projects pages

--- script/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const hamMenu = document.querySelector('.hamburgur-menu');
-  const offScreenMenu = document.querySelector('.offscreen-menu');
-
-  hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
-  });
-});
