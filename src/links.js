65f78a179b59af44d5412caa3825969bc5279472
apatheticdamn <apathetic.damn@gmail.com>
2024-04-04 20:52:31 +0530

    rename: script.js to links.js

---
+++ src/links.js
@@ -0,0 +1,28 @@
+function openWebDevHobbyLink() {
+    window.open(
+      "https://apathy-wifu-network.vercel.app/",
+      "",
+      "width=640, height=480"
+    );
+  }
+function openGameDevHobbyLink() {
+    window.open (
+      "https://github.com/apatheticdamn/pixel-dash-mai-s-Journey",
+      "",
+      "width=640, height=480"
+    )
+   }
+function openMoviesHobbyLink() {
+    window.open (
+      "https://apathetic.vercel.app/pages/movies/movies.html",
+      "",
+      "width=640, height=480"
+    )
+  }
+function openGamesHobbyLink() {
+    window.open(
+      "https://apathetic.vercel.app/pages/games/games.html",
+      "",
+      "width=640, height=480"
+    )
+  }
